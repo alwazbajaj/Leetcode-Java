5ea57a8414e07996eafa77e31563244c70d392e8
alwazbajaj <abajaj797@gmail.com>
2019-06-24 22:59:13 -0700

    Solution

--- src/Longest_substring_without_Repeating_characters.java
+++ src/Longest_substring_without_Repeating_characters.java
@@ -1,4 +1,48 @@
+import java.util.*;
 
 public class Longest_substring_without_Repeating_characters {
+	
 
-}
+        
+		public int lengthOfLongestSubstring(String s) {
+	        
+	        int res = 0 ;
+	        HashMap<Character, Integer> map = new HashMap<>();
+	        int max = 0;
+	        for (int j =0; j < s.length(); j++)
+	        {
+	        max = 0;
+	        map.clear();
+	        for (int i =j; i < s.length(); i++)
+	        {
+	            if (map.containsKey(s.charAt(i)))
+	            {
+	                if (res < max)
+	                {
+	                   res = max; 
+	                }
+	            	break;
+	            }
+	            else
+	            {
+	            	map.put(s.charAt(i), 1);
+	            	max++;
+	            }
+	        }
+	        if (res < max)
+	        {
+	            res = max;
+	        }
+	    }
+	        
+	        
+	        
+	        return res;
+	        
+	        
+	        
+	    }
+        
+    }
+
+
