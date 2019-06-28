812411be291297d668a54d528897e4cf152348cf
alwazbajaj <abajaj797@gmail.com>
2019-06-27 20:36:15 -0700

    main

---
+++ src/mainclass.java
@@ -0,0 +1,13 @@
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Longest_Prefix lp = new Longest_Prefix();
+		String[] strs = {"abc","abb"};
+		System.out.println(lp.longestCommonPrefix(strs));
+		
+
+	}
+
+}
